91c043bddb0d758d715f691b41a8e025da1f5dd6
Calin Tamas <calintamas2@gmail.com>
2023-12-04 18:55:17 +0200

    refactor day4

--- 2023/Day04.swift
+++ 2023/Day04.swift
@@ -7,84 +7,62 @@
 
 import Foundation
 
-typealias Card = (id: Int, winning: [Int], actual: [Int])
+struct Scratchcard {
+    var winning: [Int]
+    var actual: [Int]
+    
+    func score() -> Int {
+        let intersection = Set(winning).intersection(Set(actual))
+        return intersection.count
+    }
+}
+
+func extractValues(data: String?) -> [Int] {
+    guard let data = data else {
+        return []
+    }
+    return data.components(separatedBy: " ").filter({ !$0.isEmpty }).map({ $0.toInt() })
+}
 
 struct Day04: AdventDay {
-    let rows: [String]
-    var values: [Card] = []
+    var scratchcards: [Scratchcard] = []
     
     init(input: String) {
-        self.rows = input.splitByLine()
+        let rows = input.splitByLine()
         
-        for (idx, row) in rows.enumerated() {
-            let data = row.components(separatedBy: ":").last
-            if let data = data?.components(separatedBy: "|") {
-                let winningValues = Day04.extractValues(data: data.first)
-                let actualValues = Day04.extractValues(data: data.last)
-                self.values.append((idx + 1, winningValues, actualValues))
-                
-            }
+        for row in rows {
+            let data = row.components(separatedBy: ":").last!.components(separatedBy: "|")
+            
+            let winning = extractValues(data: data.first)
+            let actual = extractValues(data: data.last)
+            
+            self.scratchcards.append(Scratchcard(winning: winning, actual: actual))
         }
-    }
-    
-    static func extractValues(data: String?) -> [Int] {
-        guard let data = data else {
-            return []
-        }
-        return data.components(separatedBy: " ").filter({ !$0.isEmpty }).map({ $0.toInt() })
     }
     
     mutating func p1() -> String {
         var points = 0
-        for value in values {
-            let (_, winning, actual) = value
-            let intersection = Set(winning).intersection(Set(actual))
-            
-            let power = intersection.count - 1
-            var newPoints = 0
-            if power >= 0 {
-                newPoints = 2.pow(exponent: intersection.count - 1)
-                points += newPoints
+        for card in scratchcards {
+            let exponent = card.score() - 1
+            if exponent >= 0 {
+                points += 2.pow(exponent: exponent)
             }
         }
         return "\(points)"
     }
     
     mutating func p2() -> String {
-        var copiesDict: Dictionary<Int, Int> = [:]
-        
-        var idx = 0
-        repeat {
-            let value = values[idx]
-            
-            let (id, winning, actual) = value
-            let intersection = Set(winning).intersection(Set(actual))
-            let count = intersection.count
-            
-            var copiedCards: [Card] = []
+        var tracker = Array(repeating: 1, count: scratchcards.count)
+ 
+        for (idx, card) in scratchcards.enumerated() {
+            let count = card.score()
             
             for offset in 0..<count {
-                let copy = values[id + offset]
-                copiesDict[copy.id] = (copiesDict[copy.id] ?? 0) + 1
-                copiedCards.append(copy)
+                tracker[idx + offset + 1] += tracker[idx]
             }
-            
-            if let copies = copiesDict[id] {
-                // check how many copies we have for current card
-                for _ in 0..<copies {
-                    // and copy again each already copied card, that many times
-                    copiedCards.forEach { card in
-                        copiesDict[card.id] = (copiesDict[card.id] ?? 0) + 1
-                    }
-                }
-            }
-            
-            idx += 1
-        } while idx < values.count
+        }
         
-        let count = copiesDict.values.reduce(0, +) + values.count
-        return "\(count)"
+        let sum = tracker.sum()
+        return "\(sum)"
     }
-    
-    
 }
