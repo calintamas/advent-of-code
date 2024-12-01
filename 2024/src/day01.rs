ed2085b8065edb9fd61b8be1ec0f3690bc733d9e
Calin Tamas <calintamas2@gmail.com>
2024-12-01 22:16:39 +0200

    add 2024 day1 p2

--- 2024/src/day01.rs
+++ 2024/src/day01.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use crate::advent_day::AdventDay;
 
 pub struct Day01 {
@@ -24,6 +26,8 @@ impl AdventDay for Day01 {
             self.left.push(values[0]);
             self.right.push(values[1]);
         }
+        self.left.sort(); // sort in place
+        self.right.sort();
     }
 
     fn p1(&self) -> String {
@@ -32,14 +36,12 @@ impl AdventDay for Day01 {
     }
 
     fn p2(&self) -> String {
-        " ".to_string()
+        let sum = similarity_score(self.left.clone(), self.right.clone());
+        sum.to_string()
     }
 }
 
-fn total_distance(mut left: Vec<u64>, mut right: Vec<u64>) -> i64 {
-    left.sort(); // sort in place
-    right.sort();
-
+fn total_distance(left: Vec<u64>, right: Vec<u64>) -> i64 {
     let mut diffs: Vec<i64> = Vec::new();
 
     for (index, left_item) in left.iter().enumerate() {
@@ -56,9 +58,33 @@ fn total_distance(mut left: Vec<u64>, mut right: Vec<u64>) -> i64 {
     return sum;
 }
 
+fn similarity_score(left: Vec<u64>, right: Vec<u64>) -> u64 {
+    let mut scores: Vec<u64> = Vec::new();
+
+    let mut map: HashMap<u64, u64> = HashMap::new();
+    for &right_item in right.iter() {
+        *map.entry(right_item).or_insert(0) += 1
+    }
+
+    for left_item in left.iter() {
+        if let Some(count) = map.get(left_item) {
+            scores.push(left_item * count);
+        }
+    }
+
+    return scores.iter().sum();
+}
+
 #[test]
 fn test_total_distance() {
     let left = vec![3, 4, 2, 1, 3, 3];
     let right = vec![4, 3, 5, 3, 9, 3];
     assert_eq!(total_distance(left, right), 11);
+}
+
+#[test]
+fn test_similarity_score() {
+    let left = vec![3, 4, 2, 1, 3, 3];
+    let right = vec![4, 3, 5, 3, 9, 3];
+    assert_eq!(similarity_score(left, right), 31);
 }
