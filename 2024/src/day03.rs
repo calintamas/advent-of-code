1c40030a962327c9f03ca29d630fe7ecbb52ce16
Calin Tamas <calintamas2@gmail.com>
2024-12-03 13:08:36 +0200

    refactor parser

--- 2024/src/day03.rs
+++ 2024/src/day03.rs
@@ -29,70 +29,63 @@ impl AdventDay for Day03 {
     }
 }
 
-fn extract_p1(input: &String) -> Vec<(i32, i32)> {
-    let regex = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)").unwrap();
+enum Instruction {
+    Mul(i32, i32), // mul(X, Y)
+    Do,            // do()
+    Dont,          // don't()
+}
+
+fn parse(input: &String, re: &str) -> Vec<Instruction> {
+    let regex = Regex::new(re).unwrap();
     regex
         .captures_iter(input)
-        .map(|item| {
-            let x = item.get(1).unwrap().as_str().parse::<i32>().unwrap();
-            let y = item.get(2).unwrap().as_str().parse::<i32>().unwrap();
-            return (x, y);
+        .filter_map(|item| {
+            if let Some(x_match) = item.get(1) {
+                // match mul(X,Y)
+                let x = x_match.as_str().parse::<i32>().unwrap();
+                let y = item.get(2).unwrap().as_str().parse::<i32>().unwrap();
+                Some(Instruction::Mul(x, y))
+            } else if let Some(instr) = item.get(0) {
+                // match "do()" or "don't()"
+                match instr.as_str() {
+                    "do()" => Some(Instruction::Do),
+                    "don't()" => Some(Instruction::Dont),
+                    _ => None,
+                }
+            } else {
+                None
+            }
         })
         .collect()
 }
 
 fn run_p1(input: &String) -> i32 {
-    let pairs_to_multiply = extract_p1(input);
-    pairs_to_multiply.iter().fold(0, |mut acc, pair| {
-        acc += pair.0.mul(pair.1);
+    let re = r"mul\((\d{1,3}),(\d{1,3})\)";
+    parse(input, re).iter().fold(0, |mut acc, instr| {
+        match instr {
+            Instruction::Mul(x, y) => acc += x.mul(y),
+            _ => {}
+        }
         acc
     })
 }
 
-fn extract_p2(input: &String) -> Vec<Result<(i32, i32), String>> {
-    let regex = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)|do\(\)|don't\(\)").unwrap();
-    regex
-        .captures_iter(input)
-        .map(|item| {
-            // match mul(X,Y)
-            if let Some(x_match) = item.get(1) {
-                let x = x_match.as_str().parse::<i32>().unwrap();
-                let y = item.get(2).unwrap().as_str().parse::<i32>().unwrap();
-                Ok((x, y))
-            }
-            // match "do()" or "don't()"
-            else if let Some(do_match) = item.get(0) {
-                Err(do_match.as_str().to_string())
-            } else {
-                Err("Unknown match".to_string())
-            }
-        })
-        .collect()
-}
-
 fn run_p2(input: &String) -> i32 {
-    let instructions = extract_p2(input);
+    let re = r"mul\((\d{1,3}),(\d{1,3})\)|do\(\)|don't\(\)";
     let mut should_do = true;
-    let mut sum: i32 = 0;
 
-    for item in instructions {
-        match item {
-            Ok((x, y)) => {
+    parse(input, re).iter().fold(0, |mut acc, instr| {
+        match instr {
+            Instruction::Mul(x, y) => {
                 if should_do {
-                    sum += x.mul(y);
+                    acc += x.mul(y);
                 }
             }
-            Err(instr) => {
-                if instr == "do()" {
-                    should_do = true
-                }
-                if instr == "don't()" {
-                    should_do = false
-                }
-            }
+            Instruction::Do => should_do = true,
+            Instruction::Dont => should_do = false,
         }
-    }
-    sum
+        acc
+    })
 }
 
 #[test]
