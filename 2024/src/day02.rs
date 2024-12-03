0a1ad107f8084c2b20b2c7c6ca7167e6117059af
Calin Tamas <calintamas2@gmail.com>
2024-12-03 11:15:02 +0200

    simplify is_safe

--- 2024/src/day02.rs
+++ 2024/src/day02.rs
@@ -1,3 +1,5 @@
+use std::cmp::Ordering;
+
 use crate::advent_day::AdventDay;
 
 pub struct Day02 {
@@ -24,7 +26,7 @@ impl AdventDay for Day02 {
         let safe_count = self
             .reports
             .iter()
-            .map(|r| is_report_safe(r, 1))
+            .map(|r| is_safe(r))
             .filter(|x| *x)
             .count();
         return safe_count.to_string();
@@ -34,97 +36,72 @@ impl AdventDay for Day02 {
         let safe_count = self
             .reports
             .iter()
-            .map(|r| is_report_safe(r, 0))
+            .map(|r| is_safe_with_dampener(r))
             .filter(|x| *x)
             .count();
         return safe_count.to_string();
     }
 }
 
-fn is_report_safe(report: &Vec<u64>, iter_count: usize) -> bool {
-    let max_iter_count = 1; // The Problem Dampener allows 1 replacement
-
-    let min_diff = 1;
-    let max_diff = 3;
-
-    let diffs = report
-        .iter()
-        .enumerate()
-        .fold(Vec::<i64>::new(), |mut acc, (idx, &item)| {
-            if let Some(next_item) = report.get(idx + 1) {
-                let diff = item as i64 - *next_item as i64;
-                acc.push(diff);
-            }
-            return acc;
-        });
-
-    let mut result = true;
-    let mut idx_to_check = Vec::<usize>::new();
-
-    for (idx, &item) in diffs.iter().enumerate() {
-        if let Some(next_item) = diffs.get(idx + 1) {
-            // sign change
-            if item * next_item < 0 {
-                if iter_count >= max_iter_count {
-                    result = false;
-                    break;
-                }
-                // idx is 2-behind original report vector
-                // due to cond. above
-                idx_to_check.append(&mut vec![idx, idx + 1, idx + 2]);
-                result = false;
-            }
-        }
-
-        // diff too large
-        if item.abs() < min_diff || item.abs() > max_diff {
-            if iter_count >= max_iter_count {
-                result = false;
-                break;
-            }
-            // idx is 1-behind original report vector
-            idx_to_check.append(&mut vec![idx, idx + 1]);
-            result = false;
-        }
+fn is_safe(report: &Vec<u64>) -> bool {
+    let is_not_monotonic = match report[0].cmp(&report[1]) {
+        Ordering::Less => report.windows(2).any(|pair| pair[0] >= pair[1]),
+        Ordering::Equal => true,
+        Ordering::Greater => report.windows(2).any(|pair| pair[0] <= pair[1]),
+    };
+    if is_not_monotonic {
+        return false;
     }
 
-    for &idx in idx_to_check.iter() {
-        let mut new_report = report.clone();
-        new_report.remove(idx);
-        if is_report_safe(&new_report, iter_count + 1) {
-            result = true;
-            break;
-        }
-    }
+    let is_diff_too_large = report
+        .windows(2)
+        .any(|pair| !(1..=3).contains(&pair[0].abs_diff(pair[1])));
 
-    return result;
+    return !is_diff_too_large;
+}
+
+fn is_safe_with_dampener(report: &Vec<u64>) -> bool {
+    if !is_safe(report) {
+        for idx in 0..report.len() {
+            let mut new_report = report.clone();
+            new_report.remove(idx);
+            if is_safe(&new_report) {
+                return true;
+            }
+        }
+        return false;
+    }
+    return true;
 }
 
 #[test]
-fn test_is_report_safe() {
-    assert_eq!(is_report_safe(&vec![7, 6, 4, 2, 1], 1), true);
-    assert_eq!(is_report_safe(&vec![1, 2, 7, 8, 9], 1), false);
-    assert_eq!(is_report_safe(&vec![9, 7, 6, 2, 1], 1), false);
-    assert_eq!(is_report_safe(&vec![1, 3, 2, 4, 5], 1), false);
-    assert_eq!(is_report_safe(&vec![8, 6, 4, 4, 1], 1), false);
-    assert_eq!(is_report_safe(&vec![1, 3, 6, 7, 9], 1), true);
+fn test_is_safe() {
+    assert_eq!(is_safe(&vec![7, 6, 4, 2, 1]), true);
+    assert_eq!(is_safe(&vec![9, 7, 6, 2, 1]), false);
+    assert_eq!(is_safe(&vec![1, 2, 7, 8, 9]), false);
+    assert_eq!(is_safe(&vec![1, 3, 2, 4, 5]), false);
+    assert_eq!(is_safe(&vec![8, 6, 4, 4, 1]), false);
+    assert_eq!(is_safe(&vec![1, 3, 6, 7, 9]), true);
 }
 
 #[test]
 fn test_is_report_safe_with_dampener() {
-    assert_eq!(is_report_safe(&vec![7, 6, 4, 2, 1], 0), true);
-    assert_eq!(is_report_safe(&vec![1, 2, 7, 8, 9], 0), false);
-    assert_eq!(is_report_safe(&vec![9, 7, 6, 2, 1], 0), false);
-    assert_eq!(is_report_safe(&vec![1, 3, 2, 4, 5], 0), true);
-    assert_eq!(is_report_safe(&vec![8, 6, 4, 4, 1], 0), true);
-    assert_eq!(is_report_safe(&vec![1, 3, 6, 7, 9], 0), true);
+    assert_eq!(is_safe_with_dampener(&vec![7, 6, 4, 2, 1]), true);
+    assert_eq!(is_safe_with_dampener(&vec![1, 2, 7, 8, 9]), false);
+    assert_eq!(is_safe_with_dampener(&vec![9, 7, 6, 2, 1]), false);
+    assert_eq!(is_safe_with_dampener(&vec![1, 3, 2, 4, 5]), true);
+    assert_eq!(is_safe_with_dampener(&vec![8, 6, 4, 4, 1]), true);
+    assert_eq!(is_safe_with_dampener(&vec![1, 3, 6, 7, 9]), true);
 
     // https://www.reddit.com/r/adventofcode/comments/1h4shdu/2024_day_2_part2_edge_case_finder/
-    assert_eq!(is_report_safe(&vec![48, 46, 47, 49, 51, 54, 56], 0), true);
-    assert_eq!(is_report_safe(&vec![1, 1, 2, 3, 4, 5], 0), true);
-    assert_eq!(is_report_safe(&vec![1, 2, 3, 4, 5, 5], 0), true);
-    assert_eq!(is_report_safe(&vec![5, 1, 2, 3, 4, 5], 0), true);
-    assert_eq!(is_report_safe(&vec![1, 4, 3, 2, 1], 0), true);
-    assert_eq!(is_report_safe(&vec![1, 6, 7, 8, 9], 0), true);
-    assert_eq!(is_report_safe(&vec![1, 2, 3, 4, 3], 0), true);
+    assert_eq!(
+        is_safe_with_dampener(&vec![48, 46, 47, 49, 51, 54, 56]),
+        true
+    );
+    assert_eq!(is_safe_with_dampener(&vec![1, 1, 2, 3, 4, 5]), true);
+    assert_eq!(is_safe_with_dampener(&vec![1, 2, 3, 4, 5, 5]), true);
+    assert_eq!(is_safe_with_dampener(&vec![5, 1, 2, 3, 4, 5]), true);
+    assert_eq!(is_safe_with_dampener(&vec![1, 4, 3, 2, 1]), true);
+    assert_eq!(is_safe_with_dampener(&vec![1, 6, 7, 8, 9]), true);
+    assert_eq!(is_safe_with_dampener(&vec![1, 2, 3, 4, 3]), true);
 }
