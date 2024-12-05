cc6a1e4af45900726b6b6e28d7158317dcd7db69
Calin Tamas <calintamas2@gmail.com>
2024-12-05 10:59:55 +0200

    add 2024 day5 p2

--- 2024/src/day05.rs
+++ 2024/src/day05.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::{cmp::Ordering, collections::HashMap};
 
 use crate::advent_day::AdventDay;
 
@@ -57,28 +57,7 @@ impl AdventDay for Day05 {
         let mut sum = 0;
 
         for update in self.updates.iter() {
-            let mut valid = true;
-            for i in 0..update.len() {
-                let page = update.get(i).unwrap();
-
-                let mut other_pages = update.clone();
-                other_pages.drain(0..i + 1);
-
-                for p in other_pages.iter() {
-                    if let Some(lt_rules) = self.lt_rules.get(page) {
-                        if !lt_rules.contains(p) {
-                            valid = false;
-                            break;
-                        }
-                    } else if let Some(gt_rules) = self.gt_rules.get(page) {
-                        if gt_rules.contains(p) {
-                            valid = false;
-                            break;
-                        }
-                    }
-                }
-            }
-            if valid {
+            if update == &sort_update(update, &self.lt_rules, &self.gt_rules) {
                 sum += update.get(update.len() / 2).unwrap();
             }
         }
@@ -87,12 +66,42 @@ impl AdventDay for Day05 {
     }
 
     fn p2(&self) -> String {
-        "".to_string()
+        let mut sum = 0;
+
+        for update in self.updates.iter() {
+            let sorted_update = sort_update(update, &self.lt_rules, &self.gt_rules);
+            if update != &sorted_update {
+                sum += sorted_update.get(sorted_update.len() / 2).unwrap();
+            }
+        }
+
+        sum.to_string()
     }
 }
 
+fn sort_update(
+    update: &Vec<usize>,
+    ltr: &HashMap<usize, Vec<usize>>,
+    gtr: &HashMap<usize, Vec<usize>>,
+) -> Vec<usize> {
+    let mut update_copy = update.clone();
+    update_copy.sort_by(|a, b| {
+        if let Some(lt_rules) = ltr.get(a) {
+            if lt_rules.contains(b) {
+                return Ordering::Less;
+            }
+        } else if let Some(gt_rules) = gtr.get(a) {
+            if gt_rules.contains(b) {
+                return Ordering::Greater;
+            }
+        }
+        return Ordering::Equal;
+    });
+    update_copy
+}
+
 #[test]
-fn test_sum_middle() {
+fn test_p1() {
     let input = "47|53
 97|13
 97|61
@@ -126,4 +135,41 @@ fn test_sum_middle() {
     day.parse(input);
     let sum = day.p1();
     assert_eq!(sum, "143");
+}
+
+#[test]
+fn test_p2() {
+    let input = "47|53
+97|13
+97|61
+97|47
+75|29
+61|13
+75|53
+29|13
+97|29
+53|29
+61|53
+97|53
+61|29
+47|13
+75|47
+97|75
+47|61
+75|61
+47|29
+75|13
+53|13
+
+75,47,61,53,29
+97,61,53,29,13
+75,29,13
+75,97,47,61,53
+61,13,29
+97,13,75,29,47";
+
+    let mut day = Day05::new();
+    day.parse(input);
+    let sum = day.p2();
+    assert_eq!(sum, "123");
 }
